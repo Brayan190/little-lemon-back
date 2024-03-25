54175ad40325e888c753694db6194c6e53513e30
User <bryan07arias12@gmail.com>
2024-03-25 15:16:27 -0700

    models

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'restaurant',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+        'USER' : 'admindjango',
+        'PASSWORD' : 'employee@123!',
     }
 }
 
